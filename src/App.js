a6390a99ee21eb6d2cfb09e665a4ec0ddcd64836
Kelvin Brito <britok30@gmail.com>
2020-08-10 22:17:48 -0400

    booklist

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import BookContextProvider from './contexts/BookContext';
 import Navbar from './components/Navbar';
 
---
+++ src/components/BookList.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+import { BookContext } from '../contexts/BookContext';
+
+const BookList = () => {
+    const { books } = useContext(BookContext);
+
+    return (
+        
+    )
+};
+
+export default BookList;
